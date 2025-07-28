04b6e93345c70b817434863c7b71adcb2f05c92f
Gustavo Passos <gusapas26@gmail.com>
2025-07-28 16:58:07 -0300

    icons, sections imgs css estilizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BannerFirst } from "./components/sections/banner-first/banner-first";
+import { Header } from "./components/header/header";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BannerFirst, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/sections/banner-first/banner-first.ts
@@ -0,0 +1,17 @@
+import { Component, input, Input } from '@angular/core';
+
+import { Informacoes } from "../../informacoes/informacoes";
+import { BrilhoIcon } from "../../brilho-icon/brilho-icon";
+
+@Component({
+  selector: 'app-banner-first',
+  imports: [Informacoes, BrilhoIcon],
+  templateUrl: './banner-first.html',
+  styleUrls: ['./banner-first.css']
+})
+export class BannerFirst {
+  @Input() oferta: string = 'OPORTUNIDADE EXCLUSIVA';
+  @Input() Title: string = 'Revenda a marca de beleza mais amada do Brasil';
+  @Input() description: string = 'Transforme sua paixão por beleza em uma fonte de renda extra. Junte-se a milhares de revendedoras que já conquistaram sua independência financeira.';
+  @Input() image: string = 'assets/banner-mulher.png';
+}
