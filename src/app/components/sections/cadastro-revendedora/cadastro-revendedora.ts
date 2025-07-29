ceba087901f3a7622b38cff5ea2b6881e001dd01
Gustavo Passos <gusapas26@gmail.com>
2025-07-29 11:23:13 -0300

    validacao avacd

--- src/app/components/sections/cadastro-revendedora/cadastro-revendedora.ts
+++ src/app/components/sections/cadastro-revendedora/cadastro-revendedora.ts
@@ -27,7 +27,7 @@ export class CadastroRevendedora {
       email: ['', [Validators.required, Validators.email]],
       estado: ['', Validators.required],
       cidade: ['', [Validators.required, Validators.pattern(/^[A-Za-zÀ-ÿ\s]+$/)]],
-      numero: ['', [Validators.required, Validators.pattern(/^[\d\s\(\)\-]+$/)]],
+      numero: ['', [Validators.required, Validators.pattern(/^\(\d{2}\)\s\d{4,5}-\d{4}$/)]],
       termos: [false, Validators.requiredTrue],
     });
   }
@@ -41,5 +41,35 @@ export class CadastroRevendedora {
       console.log('Formulário inválido');
       this.cadastroForm.markAllAsTouched();
     }
+  }
+
+  isInvalid(isInvalid: string): boolean {
+    const invalid = this.cadastroForm.get(isInvalid);
+    return !!(invalid && invalid.invalid && invalid.touched);
+  }
+
+  formatarTelefone(event: any): void {
+    let valor = event.target.value;
+
+    valor = valor.replace(/\D/g, '');
+
+    if (valor.length > 11) {
+      valor = valor.slice(0, 11);
+    }
+    if (valor.length <= 2) {
+      valor = valor.replace(/(\d{0,2})/, '($1');
+    } else if (valor.length <= 7) {
+      valor = valor.replace(/(\d{2})(\d{0,5})/, '($1) $2');
+    } else {
+      valor = valor.replace(/(\d{2})(\d{5})(\d{0,4})/, '($1) $2-$3');
+    }
+    this.cadastroForm.get('numero')?.setValue(valor, { emitEvent: false });
+  }
+
+  filtrar(event: any, campo: string): void {
+    let valor = event.target.value;    
+    valor = valor.replace(/[^A-Za-zÀ-ÿ\s]/g, '');
+
+    this.cadastroForm.get(campo)?.setValue(valor, { emitEvent: false });
   }
 }
