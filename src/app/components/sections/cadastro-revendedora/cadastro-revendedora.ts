b99d1d4c6d381324f5a20e5c70d95df53cb7fe27
Gustavo Passos <gusapas26@gmail.com>
2025-07-29 10:57:29 -0300

    .

--- src/app/components/sections/cadastro-revendedora/cadastro-revendedora.ts
+++ src/app/components/sections/cadastro-revendedora/cadastro-revendedora.ts
@@ -1,4 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
   FormGroup,
@@ -6,15 +7,17 @@ import {
   Validators,
 } from '@angular/forms';
 import { Informacoes } from "../../informacoes/informacoes";
+import { EstadoBrasil } from '../../../core/models/revendedora';
 
 @Component({
   selector: 'app-cadastro-revendedora',
-  imports: [ReactiveFormsModule, Informacoes],
+  imports: [CommonModule, ReactiveFormsModule, Informacoes],
   templateUrl: './cadastro-revendedora.html',
   styleUrl: './cadastro-revendedora.css',
 })
 export class CadastroRevendedora {
   cadastroForm!: FormGroup;
+  estados = Object.entries(EstadoBrasil).map(([key, value]) => ({ sigla: key, nome: value }));
 
   private fb = inject(FormBuilder);
 
@@ -25,6 +28,16 @@ export class CadastroRevendedora {
       estado: ['', Validators.required],
       cidade: ['', Validators.required],
       numero: ['', Validators.required],
+      termos: [false, Validators.requiredTrue],
     });
+  }
+
+  onSubmit() {
+    if (this.cadastroForm.valid) {
+      console.log('Formulário válido:', this.cadastroForm.value);
+    } else {
+      console.log('Formulário inválido');
+      this.cadastroForm.markAllAsTouched();
+    }
   }
 }
--- src/app/core/models/revendedora.ts
+++ src/app/core/models/revendedora.ts
@@ -1,2 +1,21 @@
+import { EstadoBrasil } from "./enumRevendedora";
+
 export class Revendedora {
+    id: number;
+    nome: string;
+    email: string;
+    estado: EstadoBrasil;
+    cidade: string;
+    telefone: string;
+
+    constructor(id: number, nome: string, email: string, estado: EstadoBrasil, cidade: string, telefone: string) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.telefone = telefone;
+    }
 }
+export { EstadoBrasil };
+
