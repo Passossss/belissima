5ecef054d45fdb81a19558ea3392f424bba97d15
Victor Moro <morovictor733@gmail.com>
2025-07-28 21:39:58 -0300

    cadastro service & revendedora model

---
+++ src/app/core/models/revendedora.spec.ts
@@ -0,0 +1,7 @@
+import { Revendedora } from './revendedora';
+
+describe('Revendedora', () => {
+  it('should create an instance', () => {
+    expect(new Revendedora()).toBeTruthy();
+  });
+});
--- src/app/core/services/cadastro-service.ts
+++ src/app/core/services/cadastro-service.ts
@@ -1,8 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CadastroService {
+  private apiUrl = ""
   
+  constructor(private http: HttpClient){}
+
+  create(tipo: any): Observable<any>{
+    return this.http.post<any>(this.apiUrl, tipo);
+  }
+
+  getAll(): Observable<any[]>{
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getById(id: string): Observable<any>{
+    return this.http.get<any>(this.apiUrl)
+  }
+
+  update(tipo: any): Observable<any>{
+    return this.http.put<any>(`${this.apiUrl}/${tipo.id}`, tipo)
+  }
+
+  delete(id: string): Observable<void>{
+    return this.http.delete<void>(`${this.apiUrl}/${id}`)
+  }
 }
